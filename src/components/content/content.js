d8fd10718980d8079d2788fa4ed5d063247d0e20
Roman Govor 850503 <roma-govor2013@ya.ru>
2021-03-16 22:19:45 +0300

    fix: true options of context request

--- src/components/content/content.js
+++ src/components/content/content.js
@@ -1,43 +1,53 @@
 import React, { useContext } from 'react';
-import Context from '../../context';
 import './Content.scss';
-import country from '../../constants/country.json';
+import countries from '../../constants/country.json';
 import Description from './description/description';
 import Gallery, { GalleryNew } from './gallery/gallery';
 import VideoPlayer from './video/video';
 import {Map} from './map/map';
 import WeatherTimeWidgets from "./widgets/WeatherTimeWidgets";
+import {useState, useEffect} from "react";
 
 
 export default function Content(props) {
+  const [country, setCountry] = useState(props.country);
+
+  useEffect(() => {
+    if(JSON.stringify(countries[props.indexCountry]) !== country) {
+      setCountry(countries[props.indexCountry]);
+    }
+
+    console.log(country);
+  }, [country]);
+
   return (
     <React.Fragment>
-    {props.country && (
+    {country && (
       <div>
-        <WeatherTimeWidgets city={props.country.localizations[0].capital} currency = {props.country.currency} indexLang = {props.indexLang}/>
+        <WeatherTimeWidgets city={country.localizations[0].capital} currency={country.currency} indexLang={props.indexLang}/>
         <div className = 'content-country'>
-          <Description country = {props.country} indexLang = {props.indexLang}/>
-          <VideoPlayer country = {props.country}/>
-          <GalleryNew country = {props.country} indexLang = {props.indexLang}/>
+          <Description country = {country} indexLang = {props.indexLang}/>
+          <VideoPlayer country = {country}/>
+          <GalleryNew country = {country} indexLang = {props.indexLang}/>
           <Map id="Map"
             options = {{
               center: {
-                lat: props.country.center[1],
-                lng: props.country.center[0]
+                lat: country.center[1],
+                lng: country.center[0]
               },
               zoom: 5
             }}
             onMapLoad = {map => {
               const marker = new window.google.maps.Marker({
                 position: {
-                  lat: props.country.capitalLocation.coordinates[1],
-                  lng: props.country.capitalLocation.coordinates[0]
+                  lat: country.capitalLocation.coordinates[1],
+                  lng: country.capitalLocation.coordinates[0]
                 },
                 map: map,
-                title: props.country.localizations[props.indexLang].name
+                title: country.localizations[props.indexLang].name
               });
             }}
-            country = {props.country.localizations[0].name}
+            country={country.localizations[0].name}
           />
         </div>
       </div>)}
